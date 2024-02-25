2dab8dcef0280510e30dfc7ecbd364e5c0938980
timos <timoskom@gmail.com>
2024-02-25 19:15:51 +0200

    - Updated AGP to version 8.2.2 (from 8.2.1)  - Updated Google Services to version 4.4.1 (from 4.4.0)  - Updated dependencies:   - com.airbnb.android:lottie to version 6.3.0 (from 4.2.2)   - org.mockito:mockito-core to version 5.10.0 (from 5.7.0)   - org.robolectric:robolectric to version 4.11.1 (from 4.3.1)   - androidx.test.espresso:espresso-core to version 3.5.1 (from 3.1.0)  - Changed dependencies:   - org.bouncycastle:bcpkix-lts8on (version 2.72.3) to org.bouncycastle:bcpkix-jdk18on (version 1.76)   - org.bouncycastle:bcprov-lts8on (version 2.72.3) to org.bouncycastle:bcprov-jdk18on (version 1.76)

--- app/src/androidTest/java/swati4star/createpdf/ApplicationTest.java
+++ app/src/androidTest/java/swati4star/createpdf/ApplicationTest.java
@@ -3,8 +3,8 @@ package swati4star.createpdf;
 import static junit.framework.TestCase.assertEquals;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 
+import androidx.test.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
